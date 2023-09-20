3215c0f9bb5928c7f3268966c2a6cedb88d4c9d7
Mwasigwa2 <gathirimu.mwasigwa@student.moringaschool.com>
2023-09-20 21:09:01 +0300

    DONE

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# append to server/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# append to server/app.py
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
